a419691f1460af95279d7891405eacf8a6318308
Diego Ferreira <diegooferreira@gmail.com>
2022-01-27 17:42:01 -0300

    Correções dos arquivos

---
+++ src/ContaCorrente.API/Controllers/TransacaoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace ContaCorrente.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransacaoController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetTransacoesCliente(Guid clienteId)
+        {
+            return Ok();
+        }
+    }
+}
